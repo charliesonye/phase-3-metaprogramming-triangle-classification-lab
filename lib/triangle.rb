c4b807ee845305de33d9c773bf0ec3d596d467f7
Charles <c_onyemem@live.com>
2022-09-22 12:26:11 -0700

    commit tree 8db9d80a5bc14a2e3bf3fd004a8bccf47cbe9e5a

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
+
+require 'pry'
 class Triangle
   # write code here
+attr_accessor :length1, :length2, :length3
+    def initialize(length1,length2,length3)
+    @length1 =length1
+    @length2 =length2
+    @length3 =length3
+    end
+
+   def kind
+    validate_triangles
+      if @length1 == @length2 && length1 == length3
+        return :equilateral
+      elsif @length1 == @length2 || @length1 == @length3 || @length2 == @length3
+        return :isosceles
+   
+      else
+          :scalene
+      
+      end
+    end
+    def lengths_more_than_zero?
+      [length1,length2,length3].all?(&:positive?)
+    end
+
+    def proper_triangle?
+      length1 + length2 > length3 && length1 + length3 > length2 && length2 + length3 > length1
+    end
+
+
+    def validate_triangles
+      raise TriangleError unless lengths_more_than_zero? && proper_triangle?
+    end
+
+   class TriangleError <
+    StandardError
+   end
 end
